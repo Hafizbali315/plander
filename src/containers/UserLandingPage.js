bb97d397ad38cf0cc5e2e0fd3fe09201b6b7f7af
hafizbali315 <hafizbali315@gmail.com>
2022-02-12 19:35:09 +0500

    Navbar

---
+++ src/containers/UserLandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserHeader from '../components/UserHeader'
+
+const LandingPage = () => {
+	return (
+		<div className="landing_page">
+			<UserHeader />
+		</div>
+	)
+}
+
+export default LandingPage
