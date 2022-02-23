c59fe3d63c35320247323dbd949644110dc247fe
hafizbali315 <hafizbali315@gmail.com>
2022-02-23 06:10:15 +0500

    User Landing Page Header complete

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,12 +1,37 @@
 import UserNavbar from './UserNavbar'
-import Header1 from '../assets/Header1.svg'
+import HeroLeftImg from '../assets/HeroLeftImg.png'
+import WelcomePlanderMobile from '../assets/WelcomePlanderMobile.png'
+import GoggleImg from '../assets/GoggleImg.png'
+import CircleFill from '../assets/CircleFill.png'
+import AppStoreImg from '../assets/AppStoreImg.png'
 
 const UserHeader = () => {
 	return (
-		<div>
+		<>
 			<UserNavbar />
-			{/* <img src={Header1} alt="Header1" /> */}
-		</div>
+
+			<div className="hero_section">
+				<div className="left">
+					<img className="left_img" src={HeroLeftImg} alt="HeroLeftImg" />
+
+					<div className="available_buttons">
+						<button>
+							<img src={GoggleImg} alt="GoggleImg" />
+						</button>
+						<button>
+							<img src={AppStoreImg} alt="AppStoreImg" />
+						</button>
+					</div>
+				</div>
+
+				<div className="right">
+					<img className="mobile_img" src={WelcomePlanderMobile} alt="WelcomePlanderMobile" />
+					<img className="circle_fill" src={CircleFill} alt="CircleFill" />
+				</div>
+
+				<h1>Eat in a Local's Home</h1>
+			</div>
+		</>
 	)
 }
 
